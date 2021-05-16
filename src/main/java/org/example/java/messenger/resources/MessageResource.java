6efb6da54b913b09d908f7ed905588d5ce4f4226
mevluthan <58922055+mevluthan@users.noreply.github.com>
2021-05-16 03:42:36 +0300

    REST Web Hello

---
+++ src/main/java/org/example/java/messenger/resources/MessageResource.java
@@ -0,0 +1,17 @@
+package org.example.java.messenger.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/messages")
+public class MessageResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessages(){
+		return "Hello REST";
+	}
+
+}
